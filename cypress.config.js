9581958c534b997fe056cf024bd521aa0dbdb3e3
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-09-14 11:21:17 +0900

    wip

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    experimentalWebKitSupport: true,
     e2e: {
         // We've imported your old cypress plugins here.
         // You may want to clean this up later by importing these.
